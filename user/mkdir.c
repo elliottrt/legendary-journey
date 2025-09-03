713310d90851e7d2e47cff28c3583aaa7276ffc4
elliottrt <reedelliott1@icloud.com>
2025-09-03 12:17:54 -0400

    multiple inputs for mkdir

--- user/mkdir.c
+++ user/mkdir.c
@@ -2,18 +2,20 @@
 
 int main(int argc, const char **argv) {
 
-	if (argc != 2) {
-		printf("usage: %s <filename>\n", argv[0]);
+	if (argc < 2) {
+		printf("usage: %s <filename> [<filename> ...]\n", argv[0]);
 		return 1;
 	}
 
 	struct file file;
-	if (!fileopen(&file, argv[1], FCREATE | FDIRECTORY)) {
-		printf("error: unable to create %s: %m\n", argv[1]);
-		return 1;
-	}
 
-	fileclose(&file);
+	for (int i = 1; i < argc; i++) {
+		if (!fileopen(&file, argv[i], FCREATE | FDIRECTORY)) {
+			printf("error: unable to create %s: %m\n", argv[i]);
+			return 1;
+		}
+		fileclose(&file);
+	}
 	
 	return 0;
 }

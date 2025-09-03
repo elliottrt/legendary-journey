a87727218a3f1a5ec17b717235f7d534e335008f
elliottrt <reedelliott1@icloud.com>
2025-09-03 12:20:26 -0400

    add touch program

---
+++ user/touch.c
@@ -0,0 +1,21 @@
+#include "std/std.h"
+
+int main(int argc, const char **argv) {
+
+	if (argc < 2) {
+		printf("usage: %s <filename> [<filename> ...]\n", argv[0]);
+		return 1;
+	}
+
+	struct file file;
+
+	for (int i = 1; i < argc; i++) {
+		if (!fileopen(&file, argv[i], FCREATE)) {
+			printf("error: unable to create %s: %m\n", argv[i]);
+			return 1;
+		}
+		fileclose(&file);
+	}
+	
+	return 0;
+}
